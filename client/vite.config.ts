80cfaa2abc9692ecbd8aa9cb1760603b7241f9bd
granteckels <geckels1@gmail.com>
2025-05-19 17:21:03 -0500

    Initial commit

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+  server: {
+    port: 3001,
+    host: '127.0.0.1',
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+});
